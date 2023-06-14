8495049038b90e85406b398bcd271f254524b2e2
Alanduril <mariusz.stachniuk@gmail.com>
2023-06-14 17:27:28 +0200

    Adding the code

---
+++ Wyzwanie Array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Wyzwanie_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            GetOdd(array);
+            Console.WriteLine("Even numbers:");
+            GetEven(array);
+        }
+        public static void GetOdd(int[] Array)
+        {
+            foreach(int i in Array)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        public static void GetEven(int[] Array)
+        {
+            foreach (int i in Array)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
