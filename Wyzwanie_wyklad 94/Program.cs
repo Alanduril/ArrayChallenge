b002191bd79e2a7083dcbab9faf5e017c7b2710a
x1r15 <piotr.krzysztof.wyszynski@gmail.com>
2023-06-14 18:06:32 +0200

    After comments version

--- Wyzwanie_wyklad 94/Program.cs
+++ Wyzwanie_wyklad 94/Program.cs
@@ -2,96 +2,46 @@
 
 namespace Wyzwanie_wyklad_94
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            //Use var instead of named type, do not define/declare variables before they need to be used
-            string input=""; 
-            string dataType;
-            int caseIdea;
-            bool invalidString=false;
-            bool invalidInt = false;
-            bool forBoolean;
-            
             Console.WriteLine("Please enter anything You like");
-            input = Console.ReadLine();
-            char[] stringArray = input.ToCharArray();
+            var input = Console.ReadLine();
+
             Console.WriteLine("Please define the data type You used");
             Console.WriteLine("Use 1 for String\nUse 2 for Integer\nUse 3 for Boolean.");
-            dataType = Console.ReadLine();
-            caseIdea = int.Parse(dataType);
+            var hasDataTypeBeenParsed = int.TryParse(Console.ReadLine(), out var typeSymbol);
 
+            if (string.IsNullOrWhiteSpace(input) || !hasDataTypeBeenParsed || typeSymbol > 3 || typeSymbol < 1)
+            {
+                Console.WriteLine("Invalid input");
+                return;
+            }
             
-            switch (caseIdea)
+            Console.WriteLine($"You've entered {input}");
+            
+            switch (typeSymbol)
             {
                 case 1:
-                    //inconsistent naming, use English, use var 
-                    foreach(char litera in stringArray)
+                    var isStringValid = true;
+                    foreach(var character in input)
                     {
-                        //reverse if to decrease complexity
-                        if (char.IsLetter(litera))
+                        if (!char.IsLetter(character))
                         {
-                            continue;
+                            isStringValid = false;
+                            break;
                         }
-                        else
-                        {
-                           invalidString = true;
-                        }
-
                     }
-
-                    Console.WriteLine($"You've entered {input}");
-                    //when using boolean in if statement, no need to compare them to true or false 
-                    //ternary operator could be used in this case to shorten the code
-                    if (invalidString == true)
-                    {
-                        Console.WriteLine("String is invalid");
-                    }
-                    else
-                    {
-                        Console.WriteLine("It's a valid string");
-                    }
+                    Console.WriteLine(isStringValid ? "Valid String" : "Invalid String");
                     break;
                 case 2:
-                    //naming + var
-                    foreach (char cyfra in stringArray)
-                    {
-                        //reverse if
-                        if (char.IsDigit(cyfra))
-                        {                           
-                            continue;
-                        }
-                        else
-                        {                           
-                            invalidInt = true;
-                        }
-
-                    }
-                    Console.WriteLine($"You've entered {input}");
-                    //no need for == true
-                    if (invalidInt == true)
-                    {
-                        Console.WriteLine("Int is invalid");
-                    }
-                    else
-                    {
-                        Console.WriteLine("It's a valid Integer.");
-                    }
+                    var isValidNumber = int.TryParse(input, out _);
+                    Console.WriteLine(isValidNumber ? "Valid Integer" : "Invalid Integer");
                     break;
                 case 3:
-                    forBoolean = bool.Parse(input);
-                    if (forBoolean == true)
-                    {
-                        Console.WriteLine("The bool is True.");
-                    }
-                    else
-                    {
-                        Console.WriteLine("The bool is false.");
-                    }
-                    break;
-                default:
-                    Console.WriteLine("bullshitery");
+                    var isValidBoolean = bool.TryParse(input, out var booleanValue);
+                    Console.WriteLine(isValidBoolean ? $"Valid Boolean: {booleanValue}" : "Invalid Boolean");
                     break;
             }
                 Console.ReadKey();
